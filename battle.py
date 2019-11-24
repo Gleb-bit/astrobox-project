a46338f6e7f8db606dd96d6e147d2e0c0eca9113
WaD <suguby@gmail.com>
2019-11-24 20:41:41 +0300

    возможность указать пути к модулям дронов

--- battle.py
+++ battle.py
@@ -16,8 +16,8 @@ def players_choose():
     if not hangars:
         raise ValueError(f'No hangars in {PROJECT_PATH}')
     for index, path in enumerate(hangars):
-        print('\t', index, path)
-    choice = int(input('Название директории для подгрузки кода >>> '))
+        print(f'\t {index} - {path}')
+    choice = int(input('Номер директории для подгрузки кода >>> '))
     number_of_players = int(input('Количество игроков >>> '))
     hangar_path = hangars[choice] if os.path.exists(os.path.join(PROJECT_PATH, hangars[choice])) else None
     players_to_add = [x for x in os.listdir(os.path.join(PROJECT_PATH, hangar_path)) if '__' not in x]
@@ -25,7 +25,7 @@ def players_choose():
     if players_to_add:
         for number in range(1, number_of_players + 1):
             for index, path in enumerate(players_to_add):
-                print('\t', index, path)
+                print(f'\t {index} - {path}')
             index_for_pop = int(input(f"Выберите игрока №{number} >>> "))
             name = players_to_add.pop(index_for_pop)[:-3]
             added_players.append(os.path.join(hangar_path + '.' + name))
@@ -35,7 +35,7 @@ def players_choose():
     return added_players
 
 
-def run_battle(players, speed=150, asteroids_count=50, show_screen=False):
+def run_battle(players, speed=150, asteroids_count=50, drones_count=5, show_screen=False):
     scene = SpaceField(
         speed=speed,
         field=(1200, 600),
@@ -44,35 +44,42 @@ def run_battle(players, speed=150, asteroids_count=50, show_screen=False):
         headless=not show_screen,
     )
     drones_teams = {}
-    NUMBER_OF_DRONES = 5
-    names = []
-
-    for index, path_to_drone in enumerate(players):
-        name = path_to_drone.split('.')[1]
-        names.append(name)
-        drone = importlib.import_module(path_to_drone).drone_class
-        drones_teams[name] = [drone() for _ in range(NUMBER_OF_DRONES)]
+    for i, team_module in enumerate(players):
+        if '.py' in team_module:
+            team_module = team_module.replace(PROJECT_PATH, '').replace('.py', '').replace('/', '.')
+        drone = importlib.import_module(team_module).drone_class
+        drones_teams[i] = [drone() for _ in range(drones_count)]
     return scene.go()
 
 
 def print_battle_result(result):
+    print('')
+    print(f'Battle result:')
     pprint(result)
 
 
 def save_battle_result(result, path):
     with open(path, 'w') as ff:
         ff.write(json.dumps(result))
-    print(f'Battle result save to {path}')
+    print('')
+    print(f'Battle result saved to {path}')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Запускает битву нескольких игроков.')
-    parser.add_argument('-p', '--players', type=str, nargs=argparse.ONE_OR_MORE,
+    parser = argparse.ArgumentParser(
+        description='Запускает битву нескольких команд дронов. '
+                    'Модули с дронами можно указать как python-модули, так и пути к ним. '
+                    'Например: "python -m battle -p hangar_2019.module_1 hangar_2019.module_2" '
+                    'или "python -m battle -p hangar_2019/.module_1.py hangar_2019/.module_2.py" ". '
+                    'Если модули не указаны, скрипт выяснит их интерактивно '
+    )
+    parser.add_argument('-p', '--player-module', type=str, nargs=argparse.ONE_OR_MORE,
                         help='Список модулей команд игроков в формате hangar_XXXX.module_name')
-    parser.add_argument('-s', '--game-speed', type=int, default=5, help='Скорость игры')
+    parser.add_argument('-s', '--game-speed', type=int, default=5, help='Скорость битвы')
     parser.add_argument('-a', '--asteroids-count', type=int, default=10, help='Количество астероидов')
-    parser.add_argument('-o', '--out-file', type=str, help='Путь для сохранения json-результатов игры')
-    parser.add_argument('-c', '--show-screen', action='store_true', help='показать экран игры')
+    parser.add_argument('-d', '--drones-count', type=int, default=5, help='Количество дронов в команде')
+    parser.add_argument('-o', '--out-file', type=str, help='Путь для сохранения json-результатов битвы')
+    parser.add_argument('-c', '--show-screen', action='store_true', help='показать экран битвы')
     # TODO запиши себе параметры в запуск в пайчарме
     # args = parser.parse_args(''
     #                          '-p hangar_2019.kharitonov hangar_2019.vinogradov'
@@ -81,10 +88,11 @@ if __name__ == '__main__':
     #                          # ' -o /tmp/battle_1.json'
     #                          ''.split())
     args = parser.parse_args()
-    players = args.players if args.players else players_choose()
+    players = args.player_module if args.player_module else players_choose()
     try:
         result = run_battle(players=players, speed=args.game_speed,
-                            asteroids_count=args.asteroids_count, show_screen=args.show_screen)
+                            asteroids_count=args.asteroids_count, drones_count=args.drones_count,
+                            show_screen=args.show_screen)
         if result:
             if args.out_file:
                 save_battle_result(result=result, path=args.out_file)
