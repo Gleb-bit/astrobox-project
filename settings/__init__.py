d89aad903dfdb37859fda84194e2477cb8362245
WaD <suguby@gmail.com>
2019-12-26 15:40:44 +0300

    подсказка как избежать варнгинга

--- settings/__init__.py
+++ settings/__init__.py
@@ -6,4 +6,4 @@ from .base import *
 try:
     from .local import *
 except ImportError:
-    warnings.warn(f'No settings.local module. Skipping.')
+    warnings.warn(f'No settings.local module. You can make it empty to avoid this message.')
